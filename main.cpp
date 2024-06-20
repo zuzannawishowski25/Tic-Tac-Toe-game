80dae613f2afaafe0a88da991142c38f8227bc95
zuzannawishowski25 <112281335+zuzannawishowski25@users.noreply.github.com>
2024-06-20 01:49:36 -0400

    final touches

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@ using namespace std;
 
 char matrix[3][3] = { '1', '2', '3', '4', '5', '6', '7', '8', '9' };
 char player = 'X';
+int n;
 
 void Draw() {
     cout << "Tic Tac Toe v1.0" << endl;
@@ -16,42 +17,96 @@ void Draw() {
 
 void Input() {
     int a;
-    cout << "Press the number of the field: ";
+    cout <<  "It's " << player <<  "'s" <<  "turn" << "Press the number of the field: ";
     cin >> a;
 
     if (a == 1) {
-        matrix[0][0] = player;
+        if (matrix[0][0] == '1') {
+            matrix[0][0] = player;
+        }
+        else {
+            cout << "Field is taken! Try Again." << endl;
+            Input();
+        }
     }
 
     else if (a == 2) {
+        if ( matrix[0][1] == '2' ) {
         matrix[0][1] = player;
+        }
+        else {
+            cout << "Field is taken! Try Again." << endl;
+            Input();
+        }
     }
 
     else if (a == 3) {
+        if ( matrix[0][2] == '3' ) {
         matrix[0][2] = player;
+        }
+        else {
+            cout << "Field is taken! Try Again." << endl;
+            Input();
+        }
     }
     else if (a == 4) {
+        if ( matrix[1][0] == '4' ) {
         matrix[1][0] = player;
+        }
+        else {
+            cout << "Field is taken! Try Again." << endl;
+            Input();
+        }
     }
 
     else if (a == 5) {
+        if ( matrix[1][1] == '5' ) {
         matrix[1][1] = player;
+        }
+        else {
+            cout << "Field is taken! Try Again." << endl;
+            Input();
+        }
     }
 
     else if (a == 6) {
+        if ( matrix[1][2] == '6' ) {
         matrix[1][2] = player;
+        }
+        else {
+            cout << "Field is taken! Try Again." << endl;
+            Input();
+        }
     }
 
     else if (a == 7) {
+        if ( matrix[2][0] == '7' ) {
         matrix[2][0] = player;
+        }
+        else {
+            cout << "Field is taken! Try Again." << endl;
+            Input();
+        }
     }
 
     else if (a == 8) {
+        if ( matrix[2][1] == '8' ) {
         matrix[2][1] = player;
+        }
+        else {
+            cout << "Field is taken! Try Again." << endl;
+            Input();
+        }
     }
 
     else if (a == 9) {
+        if ( matrix[2][2] == '9' ) {
         matrix[2][2] = player;
+        }
+        else {
+            cout << "Field is taken! Try Again." << endl;
+            Input();
+        }
     }
 
 }
@@ -68,8 +123,6 @@ void TogglePlayer() {
 
 //checking if player won game:
 char Win() {
-
-
 /////////////// FOR X ////////////////
     // by row
     if (matrix[0][0] == 'X' && matrix[0][1] == 'X' && matrix[0][2] == 'X' ) {
@@ -137,8 +190,10 @@ char Win() {
 
 int main() {
 
+    n = 0;
     Draw();
     while (1) {
+        n++;
         Input();
         Draw();
         if (Win() == 'X') {
@@ -149,6 +204,10 @@ int main() {
             cout << "O Won!" << endl;
             break;
         }
+        else if (Win() == '/' && n == 9 ) {
+            cout << "It's a Draw!" << endl;
+        }
+
         TogglePlayer();
     }
     return 0;
