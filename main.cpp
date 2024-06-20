c6316010280e2b45ae87fac7d97e60bfc0d0591c
zuzannawishowski25 <112281335+zuzannawishowski25@users.noreply.github.com>
2024-06-20 00:08:36 -0400

    added player's input

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 
 char matrix[3][3] = { '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+char player = 'X';
 
 void Draw() {
     cout << "Tic Tac Toe v1.0" << endl;
@@ -13,9 +14,65 @@ void Draw() {
     }
 }
 
+void Input() {
+    int a;
+    cout << "Press the number of the field: ";
+    cin >> a;
+
+    if (a == 1) {
+        matrix[0][0] = player;
+    }
+
+    else if (a == 2) {
+        matrix[0][1] = player;
+    }
+
+    else if (a == 3) {
+        matrix[0][2] = player;
+    }
+    else if (a == 4) {
+        matrix[1][0] = player;
+    }
+
+    else if (a == 5) {
+        matrix[1][1] = player;
+    }
+
+    else if (a == 6) {
+        matrix[1][2] = player;
+    }
+
+    else if (a == 7) {
+        matrix[2][0] = player;
+    }
+
+    else if (a == 8) {
+        matrix[2][1] = player;
+    }
+
+    else if (a == 9) {
+        matrix[2][2] = player;
+    }
+
+}
+
+void TogglePlayer() {
+
+    if (player == 'X') {
+        player = 'O';
+    }
+    else {
+        player = 'X';
+    }
+}
+
 int main() {
 
     Draw();
-
+    while (1) {
+        Input();
+        Draw();
+        TogglePlayer();
+    }
     return 0;
 };
